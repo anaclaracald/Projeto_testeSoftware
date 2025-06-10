dd5225e9bac9fde793bd5b219f93dadf50753eeb
Alice Ferreira <alice.fandrade@a.ucb.br>
2025-06-10 18:46:59 -0300

    fix: import em app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
-import express from "express"
-import admin from "firebase-admin"
+const express = require("express")
+const admin = require("firebase-admin")
 const app = express()
 
 app.use(express.json())
